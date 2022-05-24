a47b53acf3b339af7596857d006dd110079d880b
GoldenPavilion <rcorystine@gmail.com>
2022-05-24 16:58:37 -0400

    removed data variable

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,15 +12,6 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
