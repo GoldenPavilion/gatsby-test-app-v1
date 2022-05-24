537cec4e44235aaab482bd7c6601390599b12219
GoldenPavilion <rcorystine@gmail.com>
2022-05-24 17:11:04 -0400

    created IconContext and Nav wrappers

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,9 +3,14 @@ import { FaBars, Fatimes } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 
 const NavBar = () => {
+    
     return(
         <div>
-            
+            <IconContext.Provider value={{ color: "#141414"}}>
+                <Nav active={scroll} click={click}>
+
+                </Nav>
+            </IconContext.Provider>
         </div>
     )
 }
