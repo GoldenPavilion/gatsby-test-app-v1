f3d70d1b64a340066262e2cfae144943c53bd399
GoldenPavilion <rcorystine@gmail.com>
2022-05-24 17:06:23 -0400

    created skeletal NavBar component

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { FaBars, Fatimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+
+const NavBar = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar
